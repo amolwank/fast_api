f8aa19e7daf12c7cd66802bf419587197e21b26a
Amol Wankhede <info.amol@gmail.com>
2026-01-11 19:30:05 +0530

    /metrics service added for auto-promote or rollback from argocd

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Counter, generate_latest
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
+REQUESTS = Counter("http_requests_total", "Total HTTP Requests")
 
 @app.get("/")
 def home():
@@ -15,3 +17,7 @@ async def health_check():
 @app.get("/hello/{name}")
 def say_hello(name:str):
     return {"Hello ":f"You are best {name}"}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
